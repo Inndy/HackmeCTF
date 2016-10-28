f92deae44607f057a346deebcc6373002a1b736a
Inndy <inndy.tw@gmail.com>
2016-10-28 22:55:21 +0800

    Add static resource route to python server

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from bottle import *
 
+def path(x):
+    return os.path.join(os.path.dirname(__file__), x)
+
 app = Bottle()
 
 @app.get('/')
-def index():
-    pass
+def index(id=None):
+    return static_file('/index.html', path('./front-end/dist'))
+
+@app.get('/static/<file:path>')
+def index(file):
+    return static_file(file, path('./front-end/dist/static'))
+
+app.run(server='waitress', port=8787)
