d8e2ff409ead7204850db7936f6302a169154358
Inndy <inndy.tw@gmail.com>
2016-10-29 15:31:40 +0800

    Generate random url

--- front-end/src/utils/FakeData.js
+++ front-end/src/utils/FakeData.js
@@ -7,6 +7,9 @@ const CATEGORIES = 'Misc,Web,Forensic,Reverse,Pwn,Crypto,PPC'.split(/,/g)
 const PREFIXES = 'Baby,Easy,Trivial,Hard,Hell,Very'.split(/,/g)
 const NAMES = 'Web,Guestbook,Homework,SQLi,Overflow,BoF,ROP,Heap,Calc,Math,RSA,DES,AES'.split(/,/g)
 const SUFFIX = '-ng,Rev,,,'.split(/,/g)
+const PROTOCOL = 'http://,https://'.split(/,/g)
+const DOMAIN = 'hackme.inndy.tw,security.ntu.st'.split(/,/g)
+const URL = '/data,/static,/flag,,'.split(/,/g)
 
 function generate_categories() {
   return prng.shuffle(Array.from(CATEGORIES)).slice(0, prng.rand(3) + 1)
@@ -17,9 +20,13 @@ function generate_challenge() {
   var name = `${prng.choice(PREFIXES)} ${prng.choice(NAMES)}${prng.choice(SUFFIX)}`
   var categories = generate_categories()
   var solved = prng.next() % 3 == 1
-  var url = 'https://hackme.inndy.tw/socreboard/flag'
+  var url = `${prng.choice(PROTOCOL)}${prng.choice(DOMAIN)}${prng.choice(URL)}`
   var solved_count = prng.rand(17)
   var points = prng.rand(6) * 50 + 50
+
+  for(var i = 0; i < prng.rand(2); i++)
+    url += prng.choice(URL)
+
 
   if(id == 1) {
     name = 'Flag'
